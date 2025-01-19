f8c43fab8ca881aba00e5369cfe583c4cf4880b4
Cherepanov Andrey <cherepanov.andrey944@gmail.com>
2025-01-19 13:33:24 +0500

    new and create controllers

--- app/controllers/blog_posts_controller.rb
+++ app/controllers/blog_posts_controller.rb
@@ -8,4 +8,21 @@ class BlogPostsController < ApplicationController
     rescue ActiveRecord::RecordNotFound
         redirect_to root_path # Redirect to the root path if the record is not found
     end
+
+    def new
+        @blog_post = BlogPost.new
+    end
+
+    def create
+        @blog_post = BlogPost.new(blog_post_params)
+        if @blog_post.save
+            redirect_to blog_post_path(@blog_post)
+        else
+            render :new
+        end
+    end
+
+    def blog_post_params
+        params.require(:blog_post).permit(:title, :body)
+    end
 end
